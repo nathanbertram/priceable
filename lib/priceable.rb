63b19d61a4d32303ed920cd83c61f3aef3726a9b
Nathan Bertram <nbertram@gmail.com>
2013-08-03 16:21:16 -0500

    TMP Fix for deployment

--- lib/priceable.rb
+++ lib/priceable.rb
@@ -6,13 +6,16 @@ module Priceable
   def priceable(*price_fields)
     price_fields.each do |price_field|
       suffix = SUFFIXES.detect { |suffix| self.attribute_method? "#{price_field}#{suffix}".to_sym }
-      raise ArgumentError, "Unable to find valid database field for `#{price_field}#{suffix}'" unless suffix
+
+      puts "Error: Unable to find valid database field for `#{price_field}#{suffix}'" unless suffix
+      next unless suffix
+
       define_method price_field do
         unless send("#{price_field}#{suffix}".to_sym).nil?
           send("#{price_field}#{suffix}".to_sym) / 100.0
         else
           0.0
-        end
+        extend
       end  
       define_method "#{price_field}=".to_sym do |new_price|
         send("#{price_field}#{suffix}=".to_sym, (new_price.to_f * 100).round)
